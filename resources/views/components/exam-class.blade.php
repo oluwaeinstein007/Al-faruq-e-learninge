c82a754466145a1608569a5b853a2cf30e4229f7
Dare Adewole <dare.dev.adewole@gmail.com>
2020-07-31 10:32:56 +0100

    create blade exam class

---
+++ resources/views/components/exam-class.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-success rounded-lg py-5 d-flex align-items-center flex-column justify-content-center">
+    <a href="{{ $url }}" class="block text-white">
+        <div class="h3 font-weight-bold">
+            {{ $name }}
+        </div>
+    </a>
+</div>
