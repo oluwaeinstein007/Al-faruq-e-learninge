44f617e1c6e4f1ea141ff3df9e52f0b6d40164c3
Dare Adewole <dare.dev.adewole@gmail.com>
2020-07-31 10:32:33 +0100

    create vue exam class

---
+++ resources/js/components/ExamClass.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bg-success rounded-lg py-4 d-flex align-items-center flex-column justify-content-center">
+        <a :href="url" class="block text-white">
+            <div class="h2 font-weight-bold">
+                {{ name }}
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'exam-class',
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
